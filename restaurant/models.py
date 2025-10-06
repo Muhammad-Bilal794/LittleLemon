f573c4d11b82a0591eabb6ddb5f94fccc82083d1
Muhammad-Bilal-77 <mb3454545@gmail.com>
2025-10-06 06:54:18 -0700

    Added Menu and Booking models with MySQL migration and admin registration

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,24 @@
 from django.db import models
-
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your models here.
+
+class Booking(models.Model) :
+    name=models.CharField(max_length=255)
+    no_of_guests=models.IntegerField(validators=[MinValueValidator(1),MaxValueValidator(6)])
+    BookingDate=models.DateTimeField()
+    def __str__(self):
+        return f"name: {self.name}, no of guests: {self.no_of_guests}, Booking date: {self.BookingDate}"
+        
+class Menu(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField(
+        validators=[
+            MinValueValidator(0),
+            MaxValueValidator(99999)
+        ]
+    )
+
+    def __str__(self):
+        return self.title
+    
