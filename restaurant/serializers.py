f4c4b638d5a5f08759fb4c085d3df84e90ae15fc
Muhammad-Bilal-77 <mb3454545@gmail.com>
2025-10-07 05:05:18 -0700

    feat: added BookingViewSet using ModelViewSet to enable full CRUD operations

--- restaurant/serializers.py
+++ restaurant/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import Menu
+from .models import Menu , Booking
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
@@ -12,4 +12,12 @@ class UserSerializer(serializers.ModelSerializer):
 class MenuSerializer(serializers.ModelSerializer):
     class Meta:
         model = Menu
+        fields = '__all__'
+
+
+
+
+class BookingSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Booking
         fields = '__all__'
--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Menu
 from .serializers import MenuSerializer
+from rest_framework import viewsets
+from .models import Booking
+from .serializers import BookingSerializer
 # Create your views here.
 def index(request):  
       return render(request, 'index.html', {})  
@@ -17,3 +20,10 @@ class MenuItemsView(generics.ListCreateAPIView):
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
     queryset = Menu.objects.all()
     serializer_class = MenuSerializer
+
+
+
+
+class BookingViewSet(viewsets.ModelViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
