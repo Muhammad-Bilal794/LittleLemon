f19c2a66b1aa7c10b04297a70d2d1832fb8ca97d
Muhammad-Bilal-77 <mb3454545@gmail.com>
2025-10-07 05:50:15 -0700

    Add user authentication using Djoser with JWT
    - Installed Djoser and djangorestframework-simplejwt
    - Configured JWT authentication in settings.py
    - Added Djoser URLs for registration, login, and logout
    - Verified user registration, login, and logout via API
    - Removed DRF token authentication to avoid Django 5.x compatibility issues

--- LittleLemon/settings.py
+++ LittleLemon/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'restaurant',
-    'rest_framework'
+    'rest_framework',
+    'djoser',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+DJOSER = {
+    "USER_ID_FIELD": "username",
+}
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+         'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+}
